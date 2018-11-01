78901e04472f95e322ee95c8bfdf2c8fac794cd0
catherineedwards <catherine@edwards-au.com>
2018-11-01 18:10:40 +1300

    Got three tables set up and working with all needed columns

---
+++ database_structure.md
@@ -0,0 +1,12 @@
+_Recipes_
+* id
+* recipe_name
+
+_Ingredients_
+* ing_id
+* ing_name
+* type
+
+_Shopping List_
+* item_name
+* item_location
---
+++ migrations/20181101170342_ingredients.js
@@ -0,0 +1,12 @@
+exports.up = (knex, Promise) => {
+    return knex.schema.createTable('ingredients', (table) => {
+      table.increments('ing_id').primary()
+      table.string('ing_name')
+      table.string('type')
+    })
+  }
+  
+  exports.down = (knex, Promise) => {
+    return knex.schema.dropTable('ingredients')
+  }
+  
