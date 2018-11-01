859862bf90321fc583eedc53ee92052cd61684e6
catherineedwards <catherine@edwards-au.com>
2018-11-02 10:46:42 +1300

    Created recipes and ingredients

--- seeds/recipes_ingredients.js
+++ seeds/recipes_ingredients.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('recipes_ingredients').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('recipes_ingredients').insert([
+        {id: 1, ing_id: 'rowValue1'},
+        {id: 2, ing_id: 'rowValue2'},
+        {id: 3, ing_id: 'rowValue3'}
       ]);
     });
 };
