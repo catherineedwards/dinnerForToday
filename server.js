53d7d8bf24346c4e09800cf8e84c4e45f78bfac0
tkurudi <t.kurudi209@gmail.com>
2018-11-02 10:28:50 +1300

    css

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const userRoutes = require('./routes/users')
 const server = express()
 
 // Middleware
+server.use(express.static((__dirname + '/public')));
 
-server.engine('hbs', hbs({extname: 'hbs'}),({defaultLayout: 'main'}))
+server.engine('hbs', hbs({
+    defaultLayout:'main',
+    extname:'hbs'
+}))
 server.set('view engine', 'hbs')
 server.use(express.urlencoded({extended: true}))
 
