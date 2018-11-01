e8de7ac429dd675c7791c88b39b91069c60928e1
catherineedwards <catherine@edwards-au.com>
2018-11-02 11:00:58 +1300

    Created recipes and ingredients relationships table

--- seeds/recipes_ingredients.js
+++ seeds/recipes_ingredients.js
@@ -5,9 +5,25 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('recipes_ingredients').insert([
-        {id: 1, ing_id: 'rowValue1'},
-        {id: 2, ing_id: 'rowValue2'},
-        {id: 3, ing_id: 'rowValue3'}
+        {id: 1, ing_id: '1'},
+        {id: 1, ing_id: '2'},
+        {id: 1, ing_id: '3'},
+        {id: 1, ing_id: '4'},
+        {id: 1, ing_id: '5'},
+        {id: 2, ing_id: '6'},
+        {id: 2, ing_id: '8'},
+        {id: 2, ing_id: '9'},
+        {id: 2, ing_id: '10'},
+        {id: 2, ing_id: '4'},
+        {id: 2, ing_id: '5'},
+        {id: 3, ing_id: '7'},
+        {id: 3, ing_id: '11'},
+        {id: 3, ing_id: '12'},
+        {id: 3, ing_id: '14'},
+        {id: 3, ing_id: '10'},
+        {id: 3, ing_id: '4'},
+        {id: 3, ing_id: '5'},
+        {id: 3, ing_id: '8'}
       ]);
     });
 };
